097a35d0e595b4c1d4b1d9395ee9e52da0a4b0be
Keshav Murthy <keshav.murthy@latitudefinancial.com>
2019-04-03 22:42:52 +1100

    Add coverage report.

--- scan.sh
+++ scan.sh
@@ -2,6 +2,9 @@
 
 set -eu
 
+echo "Generate coverage and test report"
+sbt coverage test
+sbt coverageReport
 echo "Scanning now..."
 # SONAR_CLOUD_TOKEN should be added in circleci project setting
 sonar-scanner -X -Dsonar.login="$SONAR_CLOUD_TOKEN"
