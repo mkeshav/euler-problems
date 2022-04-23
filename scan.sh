4476463a164c4695bbbf3abb5bd39a44ba458f0b
Keshav Murthy <mkeshav@gmail.com>
2022-04-23 21:32:22 +1000

    Coverage not working with scala3 remove uploading report to sonar for now'

--- scan.sh
+++ scan.sh
@@ -2,9 +2,6 @@
 
 set -eu
 
-echo "Generate coverage and test report"
-sbt coverage test
-sbt coverageReport
 echo "Scanning now..."
 # SONAR_CLOUD_TOKEN should be added in circleci project setting
 sonar-scanner -X -Dsonar.login="$SONAR_CLOUD_TOKEN"
